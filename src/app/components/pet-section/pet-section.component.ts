3b563839aa5c7aa853cd347152ad5f823733b307
Daniel <dqfenixtriad@gmail.com>
2024-03-27 23:33:22 -0500

    “Estructuracion”

---
+++ src/app/components/pet-section/pet-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-section.component.html',
+  styleUrl: './pet-section.component.css'
+})
+export class PetSectionComponent {
+
+}
