03a7313d416e1d417117791a247bf2055b9fb65f
JoseLuisColchaC <jcfenixtriad@gmail.com>
2024-04-03 10:54:52 -0500

    Crear tema global en tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
+    colors: {
+      purple: '#280a38', //Fondo footer y filtro de imagen en la visión y en los servicios
+      orange: '#ff7a00', //Títulos
+      'purple-light': '#d3dce6', //Iconos (footer) y rectangulos morados claros
+      'purple-opaque': '#2c0e3e', //Filtro de imagen en el header
+      'purple-super-light': '#f7f0ff', //Círculos en la sección contacto
+      'purple-degraded': '#8f00ff', //Color morado para degradado de botón y texto
+      'pink-degraded': '#fc00d3', //Color fucsia para degradado de botón y texto
+      'orange-degraded': '#ff8a00', //Color maranja para degradado de botón y texto
+      'orange-opaque': '#4f2c04', //Filtro de imagen en la misión
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'], //Descripción en cada sección
+    },
     extend: {},
   },
   plugins: [],
