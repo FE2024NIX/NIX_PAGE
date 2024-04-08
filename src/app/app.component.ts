ab4617fbffd8aab67199df820e1e62ed8531e6d1
JoseLuisColchaC <jcfenixtriad@gmail.com>
2024-04-08 16:22:52 -0500

    Cambiar ubicación de componente 'pet-section'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PetSectionComponent } from './components/pet-section/pet-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PetSectionComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
