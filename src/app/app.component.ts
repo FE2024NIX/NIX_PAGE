270154d2f1b45490cf2b18fc5ca1df0223e4edd7
Francisco Valle <francisvb92@gmail.com>
2024-05-18 12:25:29 -0500

    Integración de ramas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PetSectionComponent } from './components/pet-section/pet-section.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesComponent } from './components/services/services.component';
+import { EmpresaComponent } from './components/empresa/empresa.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PetSectionComponent, ContactComponent],
+  imports: [RouterOutlet, PetSectionComponent, ContactComponent, FooterComponent, ServicesComponent,
+    EmpresaComponent, NavbarComponent, HeaderComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
