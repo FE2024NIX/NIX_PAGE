d5b47c985899bccfcd4314e76748f73c69ce16e5
JoseLuisColchaC <jcfenixtriad@gmail.com>
2024-07-09 22:36:54 -0500

    Agregar loading principal

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,40 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER } from '@angular/core';
+
+export function appInitializerFactory(): () => Promise<void> {
+  return () => {
+    return new Promise<void>(resolve => {
+      if (document.readyState === 'complete') {
+        resolve();
+      } else {
+        window.addEventListener('load', () => resolve());
+      }
+    }).then(() => {
+      const loader = document.querySelector('.loader-container') as HTMLElement;
+      if (loader) {
+        loader.style.display = 'none';
+      }
+       document.body.classList.remove('black-background');
+    });
+  };
+}
+
+bootstrapApplication(
+  AppComponent,
+  {
+    ...appConfig,
+    providers: [
+      ...(appConfig.providers || []), // Preserva los proveedores ya definidos en appConfig
+      provideRouter([]),
+      {
+        provide: APP_INITIALIZER,
+        useFactory: appInitializerFactory,
+        multi: true,
+      },
+    ],
+  }
+).catch(err => console.error(err));
